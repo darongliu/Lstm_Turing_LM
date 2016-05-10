7bc2f724b436510899015ba2d3f32b96d7daa655
darongliu <b01901079@ntu.edu.tw>
2016-05-10 17:16:22 +0800

    minor change on script and v1-ori finished trained model

--- lstm-neural-turing-machines-lm/exp1/v1-ori/run_script/run_test.sh
+++ lstm-neural-turing-machines-lm/exp1/v1-ori/run_script/run_test.sh
@@ -3,16 +3,16 @@ train_file="../../../data/train.full"
 valid_file="../../../data/valid.full"
 test_file="../../../data/test.full"
 neuron_type="LSTM" # RNN/LSTM
-#train_method="ALL"
-train_method="LSTM"
+train_method="ALL"
+#train_method="LSTM"
 projection_size=50
 hidden_size=50
 stack_size=1
 learning_rate=0.0001
 minibatch_size=100
 max_epoch=1000
-#save_model="model/full/pro"$projection_size".h"$hidden_size".mini"$minibatch_size".neuron"$neuron_type""
-save_model="../../../lstm-lm/531-lstm/model/full/pro"$projection_size".h"$hidden_size".mini"$minibatch_size".neuron"$neuron_type""
+save_model="model/full/pro"$projection_size".h"$hidden_size".mini"$minibatch_size".neuron"$neuron_type""
+#save_model="../../../lstm-lm/531-lstm/model/full/pro"$projection_size".h"$hidden_size".mini"$minibatch_size".neuron"$neuron_type""
 #load_model=""
 
 
