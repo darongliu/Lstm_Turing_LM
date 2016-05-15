7cd212a45044dc3af2131b63f23364c87e4e556d
darongliu <b01901079@ntu.edu.tw>
2016-05-15 10:58:48 +0800

    finish training exp1 two weight model

--- lstm-neural-turing-machines-lm/exp1/v1-two-weight/run_script/run_test.sh
+++ lstm-neural-turing-machines-lm/exp1/v1-two-weight/run_script/run_test.sh
@@ -1,15 +1,15 @@
-train_file="data/train.small"
-valid_file="data/valid.small"
-test_file="data/test.small"
+train_file="../../../data/train.full"
+valid_file="../../../data/valid.full"
+test_file="../../../data/test.full"
 neuron_type="LSTM" # RNN/LSTM
-train_method="LSTM"
-projection_size=100
-hidden_size=100
+train_method="ALL"
+projection_size=50
+hidden_size=50
 stack_size=1
 learning_rate=0
 minibatch_size=100
 max_epoch=1000
-save_model="model/small/pro"$projection_size".h"$hidden_size".mini"$minibatch_size".neuron"$neuron_type""
+save_model="model/full/pro"$projection_size".h"$hidden_size".mini"$minibatch_size".neuron"$neuron_type""
 #load_model=""
 
 
@@ -17,4 +17,4 @@ save_model="model/small/pro"$projection_size".h"$hidden_size".mini"$minibatch_si
 #python lm_v4.py --train $train_file --valid $valid_file --neuron-type $neuron_type --train-method $train_method --projection-size $projection_size --hidden-size $hidden_size --stack $stack_size --learning-rate $learning_rate --minibatch-size $minibatch_size --max-epoch $max_epoch --save-net $save_model --early-stop 0
 
 # test
-python lm_v4.py --test $test_file --train-method $train_method --load-net $save_model
+THEANO_FLAGS='floatX=float32,device=gpu0' python lm_v4.py --test $test_file --train-method $train_method --load-net $save_model
