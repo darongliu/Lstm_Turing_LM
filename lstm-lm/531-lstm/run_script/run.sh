5e8f1132b5d5777e317f0dcf1991f6e290f10401
darongliu <darong@kepler.iis.sinica.edu.tw>
2016-07-19 17:19:55 +0800

    change lstm lm lr from 0.01 to 0.1

--- lstm-lm/531-lstm/run_script/run.sh
+++ lstm-lm/531-lstm/run_script/run.sh
@@ -1,15 +1,22 @@
 #! /bin/bash
-train_file="../../data/train.small"
-valid_file="../../data/valid.small"
-test_file="../../data/test.small"
+train_file="../../completion_data/data/train.txt"
+valid_file="../../completion_data/data/valid.txt"
+test_file="../../completion_data/data/test.txt"
+#train_file="../../conceptNet_proj/data/train.txt"
+#valid_file="../../conceptNet_proj/data/valid.txt"
+#test_file="../../conceptNet_proj/data/test.txt"
+#train_file="../../data/train.small"
+#valid_file="../../data/valid.small"
+#test_file="../../data/test.small"
 neuron_type="LSTM" # RNN/LSTM
 projection_size=50
 hidden_size=50
 stack_size=1
-learning_rate=0.01
+learning_rate=0.1
 minibatch_size=100
 max_epoch=10000
-save_model="model/small/pro"$projection_size".h"$hidden_size".mini"$minibatch_size".neuron"$neuron_type""
+#save_model="model/completion/pro"$projection_size".h"$hidden_size".mini"$minibatch_size".neuron"$neuron_type""
+save_model="model/completion/pro"$projection_size".h"$hidden_size".mini"$minibatch_size".neuron"$neuron_type""
 
 
 # train
